4412bf2a6e25068a1ea163f01ff76d3baaecdb40
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-04 18:37:30 -0700

    got the post route written but the response says I need to enable JavaScript

--- src/services/birds.js
+++ src/services/birds.js
@@ -1,6 +1,9 @@
 export const postBird = (bird, season) => {
     return fetch('http://localhost:7890/birds', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify({ bird, season })
     })
     .then(res => res.json());
